eed203af360604e4a2d203dea934820d96aa2d25
Daniel Long <danielwlong618@gmail.com>
2023-09-02 12:58:18 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog")
+
+function mondayWork (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+mondayWork ("work from home")
+
+function wrapAdjective (flair = "*") {
+    return function inner (message) {
+        return `You are ${flair}${message}${flair}!`
+}
+}
+wrapAdjective()("a hard worker")
+wrapAdjective("||")("a dedicated programmer")
